93ab07d471553c0618b924d8376f7ac84ea6d0fc
Thodoris Theodorou <thotheod@microsoft.com>
2025-03-07 11:28:20 +0200

    Update README.md and extratools.ps1: correct Ubuntu version to 22.02 and streamline WSL installation process

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ This repo contains simple Bicep to create a Dev Box Definition with custom Image
 ### WSL in custom image
 The custom image created by the Bicep is having WSL2 installed. The WSL2 is enabled and updated, but when you boot for the first time it asks you to update WSL again (currently to version 2.4.11).
 - to update open a terminal with administrator rights and run `wsl.exe --update`. 
-- To install UBUNTU 22.04 run `wsl.exe --install -d Ubuntu-22.04`
+- To install UBUNTU 22.04 run `wsl.exe --install -d Ubuntu-22.02`
     - Alternatively you can install UBUNTU 24.04 with `wsl.exe --install -d Ubuntu-24.04`
 
 
--- scripts/extratools.ps1
+++ scripts/extratools.ps1
@@ -1,3 +1,10 @@
+##########################
+## Install Chocolatey 
+##########################
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 
 ##########################
 ## Enable WSL
@@ -16,29 +23,22 @@ try {
 # update WSL to latest version
 Write-Host "Updating WSL to latest version..."
 try {
-    wsl --update --no-prompt 
+    wsl --update
     Write-Host "WSL updated to the latest version."
 } catch {
     Write-Host "An error occurred while updating WSL."
     Write-Host "Error details: $_"
 }
 
-## Set WSL default version to 2 (possibly not necessary step - it should default to ver 2)
-Write-Host "Setting WSL default version to 2..."
+## Set Ubuntu 22.02
+Write-Host "installing Ubuntu 22.02"
 try {
-    wsl --set-default-version 2 --no-prompt 
-    Write-Host "WSL default version set to 2."
+    wsl.exe --install -d Ubuntu-22.02
+    Write-Host "Installed Ubuntu 22.02."
 } catch {
-    Write-Host "An error occurred while setting WSL default version to 2."
+    Write-Host "An error occurred while installing Ubuntu 22.02."
     Write-Host "Error details: $_"
 }
-
-##########################
-## Install Chocolatey 
-##########################
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 ######################################
 ## Install extra Tools with Chocolatey
